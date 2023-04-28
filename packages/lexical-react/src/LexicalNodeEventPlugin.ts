7a02c7569b7b4697714b93caacc577b034e89ff7
fantactuka <fantactuka@gmail.com>
2023-04-28 12:29:57 -0400

    Event delegation for events plugin

--- packages/lexical-react/src/LexicalNodeEventPlugin.ts
+++ packages/lexical-react/src/LexicalNodeEventPlugin.ts
@@ -13,6 +13,8 @@ import {$findMatchingParent} from '@lexical/utils';
 import {$getNearestNodeFromDOMNode} from 'lexical';
 import {useEffect, useRef} from 'react';
 
+const capturedEvents = new Set<string>(['mouseenter', 'mouseleave']);
+
 export function NodeEventPlugin({
   nodeType,
   eventType,
@@ -32,36 +34,40 @@ export function NodeEventPlugin({
   listenerRef.current = eventListener;
 
   useEffect(() => {
-    return editor.registerRootListener((rootElement, prevRootElement) => {
-      const onEvent = (event: Event) => {
-        editor.update(() => {
-          const nearestNode = $getNearestNodeFromDOMNode(
-            event.target as Element,
-          );
-          if (nearestNode !== null) {
-            const targetNode = $findMatchingParent(
-              nearestNode,
-              (node) => node instanceof nodeType,
-            );
-            if (targetNode !== null) {
-              listenerRef.current(event, editor, targetNode.getKey());
-              return;
-            }
-          }
-        });
-      };
+    const isCaptured = capturedEvents.has(eventType);
 
+    const onEvent = (event: Event) => {
+      editor.update(() => {
+        const nearestNode = $getNearestNodeFromDOMNode(event.target as Element);
+        if (nearestNode !== null) {
+          const targetNode = isCaptured
+            ? nearestNode instanceof nodeType
+              ? nearestNode
+              : null
+            : $findMatchingParent(
+                nearestNode,
+                (node) => node instanceof nodeType,
+              );
+          if (targetNode !== null) {
+            listenerRef.current(event, editor, targetNode.getKey());
+            return;
+          }
+        }
+      });
+    };
+
+    return editor.registerRootListener((rootElement, prevRootElement) => {
       if (rootElement) {
-        rootElement.addEventListener(eventType, onEvent);
+        rootElement.addEventListener(eventType, onEvent, isCaptured);
       }
 
       if (prevRootElement) {
-        prevRootElement.removeEventListener(eventType, onEvent);
+        prevRootElement.removeEventListener(eventType, onEvent, isCaptured);
       }
     });
     // We intentionally don't respect changes to eventType.
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [editor]);
+  }, [editor, nodeType]);
 
   return null;
 }
